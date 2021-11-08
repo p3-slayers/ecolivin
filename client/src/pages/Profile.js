485a228c73d8babaf938eeb5e59322dfef18abd5
Elijah Romer <romere88@gmail.com>
2021-11-08 11:08:23 -0600

    got it so the development server will launch via concurrently, apparently both were trying to launch on port 3000 before- proxy in the client  is set to port 3001 so changed the server to launch on port 3001. Also added the Profile page template.

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="container">
+      {/* will inserts information/summary about the User */}
+    </div>
+  );
+};
+
+export default Profile;
