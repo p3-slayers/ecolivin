6dad2221c3414bdcd6ed6502b134d5969817d16a
valbona1992 <valbona12@gmail.com>
2021-11-08 18:23:00 -0600

    add routes pages/components for the app layout

---
+++ client/src/pages/Dashboard/Events.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Events = () => {
+  return (
+    <div className="container">
+      {/* will inserts information/summary about the User */}
+      <h1>Events PAGE</h1>
+    </div>
+  );
+};
+
+export default Events;
