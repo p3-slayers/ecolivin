9b38aee16366df143d963f5a14d3d14f9f334220
ronarceo <ronaldmarceo@gmail.com>
2021-11-08 20:19:39 -0600

    team page renders each member taken from data.js in assets folder

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { teamMembers } from '../assets/data';
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Team = () => {
   return (
-    <div>
-      {/* add info on each team member */}
-      <h1>TEAM BIO PAGE</h1>
+    <div className="container">
+      <h1>Our Team</h1>
+          {teamMembers.map((member) => (
+            <div className="container">
+              <div className="row">
+                <div className="col">
+                <img
+                  alt="profile"
+                  src={member.image}
+                />
+                </div>
+                <div className="col">
+                <h3> {member.name} </h3>
+                <p> {member.bio} </p>
+                </div>
+                <div className="col">
+                <div className="row">
+                <a href={member.linkedin} target="_blank" rel="noreferrer">
+                    <FaLinkedinIn />
+                </a>
+                </div>
+                <div className="row">
+                <a href={member.github} target="_blank" rel="noreferrer">
+                    <FaGithub />
+                </a>
+                </div>
+                </div>
+              </div>
+              </div>
+          ))}
+
     </div>
   );
 };
