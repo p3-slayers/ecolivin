18c2b1f4c1a2cd838fef7223c884b5fe484ec324
valbona1992 <valbona12@gmail.com>
2021-11-10 18:40:02 -0600

    updated feed with query

--- client/src/pages/Dashboard/Feed.js
+++ client/src/pages/Dashboard/Feed.js
@@ -1,11 +1,34 @@
 import React, { useState } from 'react';
+import { useQuery } from '@apollo/client';
 
-const Feed = ( {useState}) => {
-  const [text, setText] = useState('');
+import { QUERY_ALL_POSTS } from '../../utils/queries';
+
+const Feed = () => {
+  console.log("before querying");
+  const { loading, data } = useQuery(QUERY_ALL_POSTS);
+  const posts = data?.posts || [];
+  console.log("myposts:", posts);
   return (
     <div className="container">
-      {/* will inserts information/summary about the User */}
-      <h1>FEED PAGE</h1>
+      {/* will inserts about the post */}
+      <h1 className="large text-primary">Posts</h1>
+      
+      
+      <div className="posts">
+        {posts.map((post) => (
+
+          <div className="post bg-white p-1 my-1">
+            
+            <div>
+            <p className="my-1">{post.text}</p>
+            <p className="my-1">{post.user.firstName}</p>
+              <p className="post-date">Posted on {post.date}</p>
+            </div>
+          </div>
+
+
+        ))}
+      </div>
     </div>
   );
 };
