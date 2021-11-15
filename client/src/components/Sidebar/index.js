39370c06a19e6253acb639b177ca3d6ae93b27c7
Janet Iqal <j.iqal35@gmail.com>
2021-11-14 18:48:47 -0600

    styled side bar slightly by giving it a border, refrenced state to include user name and possibly user  profile img in the side bar and added a button to take user back to dahsboard

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { ListGroup, ListGroupItem, Row, Col } from "react-bootstrap";
+import { ListGroup, ListGroupItem, Row, Col, Container } from "react-bootstrap";
 import { Button } from "react-bootstrap";
+import { useGlobalUserContext } from '../../utils/GlobalState';
+
 
 function Sidebar() {
+  const [state, dispatch] = useGlobalUserContext();
+  console.log("mystate", state);
   return (
+    <div style={{border:'1px solid black' ,height:'60vh'}}>
     <Row>
       <Col xs={6} md={3} lg={3} style={{ width: "auto" }}>
         <ListGroup style={{ textAlign: "center", textDecoration: "none" }}>
+        <ListGroupItem>
+        <img src={state.profileImage}/>
+        <h5>{state.firstName} {state.lastName}</h5>
+        </ListGroupItem>
           <ListGroupItem>
             <Button style={{ width: "100%"}}>
               <Link
@@ -49,10 +58,20 @@ function Sidebar() {
               </Link>
             </Button>
           </ListGroupItem>
+          <ListGroupItem>
+            <Button style={{ width: "100%" }}>
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to="/dashboard"
+              >
+               Dashboard
+              </Link>
+            </Button>
+          </ListGroupItem>
         </ListGroup>
       </Col>
     </Row>
-    // </Container>
+    </div>
   );
 }
 
