8663f81846afca84aedf8b7e2545a1d2ee994bd1
valbona1992 <valbona12@gmail.com>
2021-11-16 20:30:37 -0600

    add text to begin questionnaire page

--- client/src/pages/Questionnaire/BeginQuestionnaire.js
+++ client/src/pages/Questionnaire/BeginQuestionnaire.js
@@ -7,7 +7,7 @@ const BeginQuestionnaire = () => {
     <div className="container text-center mt-5 pt-5">
       {/* will inserts information/summary about the User */}
       <h1>Begin Questionnaire</h1>
-      <p className="my-4">Lorem ipsum dolor sit amet. Sed culpa quia ex neque aliquam et labore placeat non mollitia maiores est omnis recusandae ex commodi obcaecati quisquam aperiam. Ut modi alias vel ipsam quidem aut.</p>
+      <p className="my-4"> You will begin a questionnaire broken down into five categories, with questions for each category. Please rate how strongly you agree or disagree with each statement.</p>
       <Link to="/questionnaire"><Button variant="primary" className="mt-4" size="lg">Start</Button>{' '}</Link>
     </div>
   );
