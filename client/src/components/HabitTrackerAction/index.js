38cab97261f6095f0e5b9d532637d808f2a5f15d
valbona1992 <valbona12@gmail.com>
2021-11-16 20:49:18 -0600

    changed trash can color to red/burgundy color

--- client/src/components/HabitTrackerAction/index.js
+++ client/src/components/HabitTrackerAction/index.js
@@ -4,12 +4,14 @@ import React, { useState } from "react";
 import Card from 'react-bootstrap/Card'
 import HabitTracker from "../../pages/Dashboard/HabitTracker";
 import {Row, Col, Form, Button} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 function HabitTrackerAction(input) {
   const text = input.text;
   const actionText = input.actionText;
   const actionId = input.actionId;
-
+  const element = <FontAwesomeIcon icon={faTrash} />
   
   const handleFormChange = (id) => (e) => {
     localStorage.setItem(id, e.target.checked);
@@ -34,9 +36,9 @@ function HabitTrackerAction(input) {
         <Col md={4} className="px-4">        
         
             {text} 
-        
+            
         <Button className="mx-2 text-decoration-none btn-light p-2" variant="link" size="sm" onClick={remove}>
-            🗑️
+             {element}
         </Button>
 
         </Col>
