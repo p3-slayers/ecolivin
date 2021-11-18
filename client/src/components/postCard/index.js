7e1ee3397d6c527ba14b568d195c1d08c571f34a
Elijah Romer <romere88@gmail.com>
2021-11-18 11:28:00 -0600

    added profileImage attribute to the client QUERY_ALL_POSTS mutation. Plugged in the profileImage as the src attribute for the newly added profile pictures on postCards. Works as advertised.

--- client/src/components/postCard/index.js
+++ client/src/components/postCard/index.js
@@ -1,7 +1,4 @@
 import React from "react";
-import { useGlobalUserContext } from '../../utils/GlobalState';
-// import { Link } from "react-router-dom";
-// import Card from 'react-bootstrap/Card'
 import Moment from 'react-moment';
 Moment.globalFormat = 'D/MM/YYYY H:MM';
 
@@ -9,17 +6,11 @@ Moment.globalFormat = 'D/MM/YYYY H:MM';
 function PostCard(input) {
   const post = input.post;
 
-  const [state] = useGlobalUserContext();
-
-  const { profileImage } = state;
-
   return (
-
-
     <div className="card text-dark bg-light mb-4 mx-5">
         <div className="card-header d-flex">
           <img 
-          src={profileImage} 
+          src={post.user.profileImage} 
           className="img-fluid img-thumbnail rounded-circle"
           id="imgIcon"
           alt="" />
@@ -33,7 +24,6 @@ function PostCard(input) {
         <p className="card-text">{post.post}</p>
         </div>
     </div>
-    
   );
 }
 
