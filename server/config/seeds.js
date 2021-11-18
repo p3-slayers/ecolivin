67e5e8901fec8c9d934df31421071761968ee8d3
Elijah Romer <romere88@gmail.com>
2021-11-18 15:16:45 -0600

    Isolated userData to a separate file. Discovered that the Mongoose insertMany method does not trigger Mongoose pre hooks, and thus all the users being entered into the database did not have hashed passwords... that meant that when you tried to log in under a seeded account, the validation would fail as the unhashed version of the password does not match the hashed version. Imported bcrypt and used for each to hash the password on each user object in the array, then awaited Promise.all() for the loop to continue. This successfully returned an array of users with hashed passwords, allowing the bulk insert.

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -14,6 +14,10 @@ const {
   Challenge
 } = require('../models');
 
+const bcrypt = require('bcrypt');
+
+
+
 db.once('open', async () => {
   await Category.deleteMany();
 
@@ -27,217 +31,27 @@ db.once('open', async () => {
 
   console.log('categories seeded');
 
-  
-  // await Action.deleteMany();
-
-  // const actions = await Action.insertMany([
-  //   {
-  //     actionId: 't1',
-  //     name: 'Carpool',
-  //     description: 'Carpool to work/school or any event with a friend',
-  //     category: categories[0]._id,
-  //   },
-  //   {
-  //     actionId: 't1',
-  //     name: 'Public Transit',
-  //     description: 'Take public transit to work or school when possible',
-  //     category: categories[0]._id,
-  //   },
-  //   {
-  //     actionId: 't1',
-  //     name: 'Bike',
-  //     description: 'Bike to a given destination instead of driving',
-  //     category: categories[0]._id,
-  //   },
-  //   {
-  //     actionId: 't1',
-  //     name: 'Plant-based Diet',
-  //     description:
-  //       'Switch to a 100% plant-based diet on a give day of the week.',
-  //     category: categories[1]._id,
-  //   },
-  //   {
-  //     actionId: 't1',
-  //     name: 'Compost',
-  //     description: 'Begin composting any food waste',
-  //     category: categories[1]._id,
-  //   },
-  //   {
-  //     actionId: 't1',
-  //     name: 'Veggie Scraps',
-  //     description:
-  //       'Reuse veggie scraps and make broths or freeze for new recipes',
-  //     category: categories[1]._id,
-  //   },
-  //   {
-  //     actionId: 't1',
-  //     name: 'Recycle',
-  //     description:
-  //       'Recycle products correctly, following your city/location guidelines',
-  //     category: categories[2]._id,
-  //   },
-  //   {
-  //     actionId: 't1',
-  //     name: 'Water Bottle',
-  //     description: 'Use a refillable water bottle instead of plastic bottles',
-  //     category: categories[2]._id,
-  //   },
-  //   {
-  //     actionId: 't1',
-  //     name: 'Reusable cups',
-  //     description: 'Bring a reusable cup to a coffee/tea shop to reduce waste',
-  //     category: categories[2]._id,
-  //   },
-  //   {
-  //     actionId: 't1',
-  //     name: 'Shower',
-  //     description:
-  //       'Decrease shower time to 5 minutes, and switch from taking baths',
-  //     category: categories[3]._id,
-  //   },
-  //   {
-  //     actionId: 't1',
-  //     name: 'Washer',
-  //     description: 'Wash clothing on cold cycle insead of hot',
-  //     category: categories[3]._id,
-  //   },
-  //   {
-  //     actionId: 't1',
-  //     name: 'Air-dry',
-  //     description:
-  //       'Air-dry clothing after washing instead of usering the drying machine',
-  //     category: categories[3]._id,
-  //   },
-  //   {
-  //     actionId: 't1',
-  //     name: 'Lighting',
-  //     description: 'Turn off lights when not in use',
-  //     category: categories[3]._id,
-  //   },
-  //   {
-  //     actionId: 't1',
-  //     name: 'Unplug Electronics',
-  //     description: 'Unplug electronics when not in use.',
-  //     category: categories[3]._id,
-  //   },
-  //   {
-  //     actionId: 't1',
-  //     name: 'Lighting',
-  //     description: 'Turn off lights when not in use',
-  //     category: categories[3]._id,
-  //   },
-  //   {
-  //     actionId: 't1',
-  //     name: 'Used Items',
-  //     description: 'But mostly used stuff and shop at second-hand stpres',
-  //     category: categories[4]._id,
-  //   },
-  //   {
-  //     actionId: 't1',
-  //     name: 'Towels and Napkins',
-  //     description:
-  //       'Switch from single use paper towels and napkins to cloth towels and handkerchief',
-  //     category: categories[4]._id,
-  //   },
-  // ]);
-
-  // console.log('action seeded');
-
   // Seeds for Users
   await User.deleteMany();
 
-  const pamela = await User.create({
-    firstName: 'Pamela',
-    lastName: 'Washington',
-    email: 'pamela@testmail.com',
-    password: 'password12345',
-    profileImage: 'https://fullstacknomadimages.s3.us-east-2.amazonaws.com/shannonMcGee.jpg-1632424486826.jpg',
-    contacts: [
-      {
-        email: "eholt@testmail.com",
-        name: "Elijah Holt"
-      },
-      {
-        email: "brian@testmail.com",
-        name: "Brian James"
-      }
-    ],
-    // answers: [
-    //   {
-    //     answer: 1,
-    //     questions: [Questionnaire[0]],
-    //   },
-    // ],
-    // actionAnswer: [
-    //   {
-    //     mondayAnswer: 10,
-    //     action: [Action[0]],
-    //   },
-    // ],
-  });
+  const userData = require(`./userData.js`)
+  const saltRounds = 10;
 
-  const elijah = await User.create({
-    firstName: 'Elijah',
-    lastName: 'Holt',
-    email: 'eholt@testmail.com',
-    password: 'password12345',
-    profileImage: 'https://fullstacknomadimages.s3.us-east-2.amazonaws.com/davidRosenkrantz.jpg-1632424420623.jpg',
-    contacts: [
-      {
-        email: "pamela@testmail.com",
-        name: "Pamela Washington"
-      },
-      {
-        email: "brian@testmail.com",
-        name: "Brian James"
-      }
-    ],
-    // answers: [
-    //   {
-    //     answer: 1,
-    //     questions: [Questionnaire[0]],
-    //   },
-    // ],
-    // actionAnswer: [
-    //   {
-    //     mondayAnswer: 10,
-    //     action: [Action[0]],
-    //   },
-    // ],
-  });
+  const usersWithHashedPasswordsPromiseArray = userData.map(
+    async (user) => {
+    let hashedPassword = await bcrypt.hash(user.password, saltRounds);
+    user.password = hashedPassword;
+    return user;
+  })
 
-  const brian = await User.create({
-    firstName: 'Brian',
-    lastName: 'James',
-    email: 'brian@testmail.com',
-    password: 'password12345',
-    // profileImage: 'insertimage.png',
-    contacts: [
-      {
-        email: "pamela@testmail.com",
-        name: "Pamela Washington"
-      },
-      {
-        email: "eholt@testmail.com",
-        name: "Elijah Holt"
-      }
-    ],
-    // answers: [
-    //   {
-    //     answer: 1,
-    //     questions: [Questionnaire[0]],
-    //   },
-    // ],
-    // actionAnswer: [
-    //   {
-    //     mondayAnswer: 10,
-    //     action: [Action[0]],
-    //   },
-    // ],
-  });
-  console.log(pamela);
+  const usersWithHashedPasswords = await Promise.all(usersWithHashedPasswordsPromiseArray)
+
+  const users = await User.insertMany(usersWithHashedPasswords)
+
 
   console.log('users seeded');
+  console.log(users)
+
 
   await Conversation.deleteMany();
 
@@ -273,22 +87,53 @@ db.once('open', async () => {
   // seeds for posts
   await Post.deleteMany();
 
-  const posts = await Post.insertMany([
-    {
-        post: "hello worlds",
-        date: + new Date(),
-        user: pamela._id,
-        comments: [],
-        likes: []
-    },
-    {
-      post: "this is a second post ",
+  const postTextSeeds = [
+    "Me: \"Do you have vegan options?\"\nWaitress: \"is vegetarian food okay?\"\nMe: \"Is monopoly money okay?\"",
+
+    "A small ATM room having two ACs and 4 tubelights, working 24/7, is asking me not to print a receipt to save the environment.",
+
+    "’m tired of people telling me to turn off my lights to save the environment. I tried it once, and nearly killed some bloke on a bike.",
+
+    "We all have to do our part for the environment. And there are many different ways one can save energy. I normally use the couch.",
+
+    "Me: I had a vegan hot dog\nThem: Why do you always mention you're vegan...\nMe next time: I had a hot dog\nThem: WHAT?! I THOUGHT YOU WERE VEGAN?!",
+
+    "The hardest part about being vegan? \n\nTbh, it's having to wake up at 5AM to milk the almonds.",
+
+    "If you meet someone new, start talking about global warming. It’s a real icebreaker.",
+
+    "If Mac users care more about the environment than Windows users, then why do Macs have a trash can and Windows has a recycling bin?."
+  ]
+
+  users.forEach(async (user, index) => {
+
+    const postFormat = {
+      post: postTextSeeds[index],
       date: + new Date(),
-      user: pamela._id,
+      user: user._id,
       comments: [],
       likes: []
-  },
-  ]);
+    }
+
+    await Post.create(postFormat)
+  })
+
+  // const posts = await Post.insertMany([
+  //   {
+  //       post: "hello worlds",
+  //       date: + new Date(),
+  //       user: pamela._id,
+  //       comments: [],
+  //       likes: []
+  //   },
+  //   {
+  //     post: "this is a second post ",
+  //     date: + new Date(),
+  //     user: pamela._id,
+  //     comments: [],
+  //     likes: []
+  // },
+  // ]);
 
   console.log('posts seeded');
 
@@ -307,7 +152,7 @@ db.once('open', async () => {
 
   console.log('results seeded');
 
-
+  process.exit();
   // seed challenges
 
   await Challenge.deleteMany();
---
+++ server/config/userData.js
@@ -0,0 +1,138 @@
+module.exports = [
+  {
+    firstName: 'Pamela',
+    lastName: 'Washington',
+    email: 'pamela@testmail.com',
+    password: 'password12345',
+    profileImage: 'https://fullstacknomadimages.s3.us-east-2.amazonaws.com/shannonMcGee.jpg-1632424486826.jpg',
+    contacts: [
+      {
+        email: "eholt@testmail.com",
+        name: "Elijah Holt"
+      },
+      {
+        email: "brian@testmail.com",
+        name: "Brian James"
+      }
+    ],
+  },
+  {
+    firstName: 'Elijah',
+    lastName: 'Holt',
+    email: 'eholt@testmail.com',
+    password: 'password12345',
+    profileImage: 'https://fullstacknomadimages.s3.us-east-2.amazonaws.com/davidRosenkrantz.jpg-1632424420623.jpg',
+    contacts: [
+      {
+        email: "pamela@testmail.com",
+        name: "Pamela Washington"
+      },
+      {
+        email: "brian@testmail.com",
+        name: "Brian James"
+      }
+    ],
+  },
+  {
+    firstName: 'Brian',
+    lastName: 'James',
+    email: 'brian@testmail.com',
+    password: 'password12345',
+    // profileImage: 'insertimage.png', - no profile pic to demonstrate default
+    contacts: [
+      {
+        email: "pamela@testmail.com",
+        name: "Pamela Washington"
+      },
+      {
+        email: "eholt@testmail.com",
+        name: "Elijah Holt"
+      }
+    ],
+  },
+  {
+    firstName: 'Ishaan',
+    lastName: 'Acharya',
+    email: 'ishaan@testmail.com',
+    password: 'password12345',
+    profileImage: 'https://fullstacknomadimages.s3.us-east-2.amazonaws.com/ishaanAcharya.jpg-1632424478301.jpg',
+    contacts: [
+      {
+        email: "pamela@testmail.com",
+        name: "Pamela Washington"
+      },
+      {
+        email: "eholt@testmail.com",
+        name: "Elijah Holt"
+      }
+    ],
+  },
+  {
+    firstName: 'Shannon',
+    lastName: 'McGee',
+    email: 'shannon@testmail.com',
+    password: 'password12345',
+    profileImage: 'https://fullstacknomadimages.s3.us-east-2.amazonaws.com/shannonMcGee.jpg-1632424486826.jpg',
+    contacts: [
+      {
+        email: "pamela@testmail.com",
+        name: "Pamela Washington"
+      },
+      {
+        email: "eholt@testmail.com",
+        name: "Elijah Holt"
+      }
+    ],
+  },
+  {
+    firstName: 'Song',
+    lastName: 'Quingling',
+    email: 'song@testmail.com',
+    password: 'password12345',
+    profileImage: 'https://fullstacknomadimages.s3.us-east-2.amazonaws.com/songQingling.jpg-1632424495831.jpg',
+    contacts: [
+      {
+        email: "pamela@testmail.com",
+        name: "Pamela Washington"
+      },
+      {
+        email: "eholt@testmail.com",
+        name: "Elijah Holt"
+      }
+    ],
+  },
+  {
+    firstName: 'Taylor',
+    lastName: 'Davies',
+    email: 'taylor@testmail.com',
+    password: 'password12345',
+    profileImage: 'https://fullstacknomadimages.s3.us-east-2.amazonaws.com/taylorDavies.jpg-1632424503228.jpg',
+    contacts: [
+      {
+        email: "pamela@testmail.com",
+        name: "Pamela Washington"
+      },
+      {
+        email: "eholt@testmail.com",
+        name: "Elijah Holt"
+      }
+    ],
+  },
+  {
+    firstName: 'Taylor',
+    lastName: 'Davies',
+    email: 'anatoly@testmail.com',
+    password: 'password12345',
+    profileImage: 'https://fullstacknomadimages.s3.us-east-2.amazonaws.com/anatolyPetrov.jpg-1632424519840.jpg',
+    contacts: [
+      {
+        email: "pamela@testmail.com",
+        name: "Pamela Washington"
+      },
+      {
+        email: "eholt@testmail.com",
+        name: "Elijah Holt"
+      }
+    ],
+  },
+];
