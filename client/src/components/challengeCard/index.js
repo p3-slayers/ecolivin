dc28aa6b0eb64a90b69603924f75cd8b4ba5d8ce
Elijah Romer <romere88@gmail.com>
2021-11-18 17:44:15 -0600

    Amended the challengeCard component so that it's output date is more readable

--- client/src/components/challengeCard/index.js
+++ client/src/components/challengeCard/index.js
@@ -3,19 +3,20 @@ import { Button } from "react-bootstrap";
 // import { Link } from "react-router-dom";
 // import Card from 'react-bootstrap/Card'
 import Moment from 'react-moment';
-Moment.globalFormat = 'D/MM/YYYY H:MM';
+// Moment.globalFormat = 'D/MM/YYYY H:MM';
+Moment.globalFormat = 'LL';
 
 
 
-function ChallengeCard(input) {
-  input = input.challenge;
-  console.log("mddskldjs", input);
-  const title = input.title;
-  // const link = input.link;
-  const challengeText = input.challenge;
-  const challengeId = input.challengeId;
-  const dateStart = input.dateStart;
-  const dateEnd = input.dateEnd;
+function ChallengeCard(props) {
+  props = props.challenge;
+  console.log("mddskldjs", props);
+  const title = props.title;
+  // const link = props.link;
+  const challengeText = props.challenge;
+  const challengeId = props.challengeId;
+  const dateStart = props.dateStart;
+  const dateEnd = props.dateEnd;
 
   const joined = localStorage.getItem(challengeId) || false;
   const [disable, setDisable] = useState(false);
@@ -49,7 +50,7 @@ function ChallengeCard(input) {
 
     <div className="card text-dark bg-light mb-4 mx-5">
         <div className="card-header"><h4> {title} - {challengeText} </h4>
-        <p className="text-muted small m-0 p-0">Dates: <Moment unix>{dateStart/1000}</Moment> - <Moment unix>{dateEnd/1000}</Moment></p>
+        <p className="text-muted small m-0 p-0">Beginning <Moment unix>{dateStart/1000}</Moment> ... Ending <Moment unix>{dateEnd/1000}</Moment></p>
 
         </div>
         <div className="card-body bg-white">
