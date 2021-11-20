b7ba8a78afc31d4d5ee6b1fa9d4b3981bd1350d4
Elijah Romer <romere88@gmail.com>
2021-11-19 20:32:18 -0600

    changed transition duration back to 400ms

--- client/src/utils/pageTransitions.js
+++ client/src/utils/pageTransitions.js
@@ -1,7 +1,7 @@
 const pageTransitions = {
   from: { opacity: 0 },
   to: { opacity: 1 },
-  config: { delay: 0, duration: 1000 },
+  config: { delay: 0, duration: 400 },
 }
 
 export default pageTransitions;
